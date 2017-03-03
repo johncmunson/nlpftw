b3e0cd6d7b68e535da3bdc187ddce504f6f14344
John Munson <johncmunson91@gmail.com>
2017-03-03 16:39:55 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Table from './Table.js'
+
+class App extends Component {
+    render() {
+        return (
+            <Table textinput='the quick brown fox jumped over the lazy dog'/>
+        );
+    }
+}
+
+export default App;
