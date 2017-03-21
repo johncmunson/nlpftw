28e6b1ff34c0aea3e6753b910f720f18331d67e0
John Munson <johncmunson91@gmail.com>
2017-03-21 17:45:27 -0400

    create a renderRows function in the table component

--- client/src/components/NLPTable.js
+++ client/src/components/NLPTable.js
@@ -1,27 +1,33 @@
 import React from 'react'
 
+function renderRows(props) {
+    return (
+        <div>
+            <tr>
+                <td><b>tokens</b></td>
+                {props.analysis.data[1].tokens.map((t, i) => (
+                    <td key={i}>
+                        {t.text.content}
+                    </td>
+                ))}
+            </tr>
+            <tr>
+                <td><b>part of speech</b></td>
+                {props.analysis.data[1].tokens.map((t, i) => (
+                    <td key={i}>
+                        {t.partOfSpeech.tag}
+                    </td>
+                ))}
+            </tr>
+        </div>
+    )
+}
+
 const NLPTable = (props) => (
     <table>
         <tbody>
             {props.analysis.data ? (
-                <div>
-                    <tr>
-                        <td><b>tokens</b></td>
-                        {props.analysis.data[1].tokens.map((t, i) => (
-                            <td key={i}>
-                                {t.text.content}
-                            </td>
-                        ))}
-                    </tr>
-                    <tr>
-                        <td><b>part of speech</b></td>
-                        {props.analysis.data[1].tokens.map((t, i) => (
-                            <td key={i}>
-                                {t.partOfSpeech.tag}
-                            </td>
-                        ))}
-                    </tr>
-                </div>
+                renderRows(props)
             ) : (
                 null
             )}
