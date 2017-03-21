efee64d623eca3fb02c640dc75bf2469990018c0
John Munson <johncmunson91@gmail.com>
2017-03-21 16:54:51 -0400

    cleanup server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Language = require('@google-cloud/language')
 
 const app = module.exports = express()
 
 app.use(cors())
 app.use(bodyParser.json())
-
-// Imports the Google Cloud client library
-const Language = require('@google-cloud/language')
 
 // Your Google Cloud Platform project ID
 const projectId = 'YOUR_PROJECT_ID'
@@ -21,7 +19,6 @@ const languageClient = Language({
 app.post('/api/analyze', function(req, res) {
     languageClient.annotate(req.body.content)
         .then(results => {
-            console.log(results)
             res.status(200).json(results)
         })
 })
