a923167358a7632744129daf6475fc1c508452c2
John Munson <johncmunson91@gmail.com>
2017-03-22 16:05:35 -0400

    prep analyzeContent.js to make concurrent requests

--- client/src/actions/analyzeContent.js
+++ client/src/actions/analyzeContent.js
@@ -3,17 +3,27 @@ import requestingAnalysis from './requestingAnalysis.js'
 import receiveAnalysis from './receiveAnalysis.js'
 import analysisError from './analysisError.js'
 
+function fetchGoogleAnalysis(content) {
+    return axios({
+        method: 'post',
+        url: `/api/analyze`,
+        data: {content}
+    })
+}
+
 export default function analyzeContent(content) {
     return function(dispatch) {
         dispatch(requestingAnalysis())
-        return axios({
-            method: 'post',
-            url: `/api/analyze`,
-            data: {content}
-        })
-            .then(response => {
-                dispatch(receiveAnalysis(response))
-            })
+        // Make requests concurrently
+        axios.all([
+            fetchGoogleAnalysis(content),
+            axios.get('https://api.github.com/users/codeheaven-io')
+        ])
+            // All requests are now complete
+            .then(axios.spread(function(googleAnalysis, users) {
+              dispatch(receiveAnalysis(googleAnalysis))
+              console.log(users)
+            }))
             .catch(err => dispatch(analysisError(err)))
     }
 }
